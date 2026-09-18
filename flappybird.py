dcb63a318417a108dfcb3edfbbf27906adbb3e15
s4251447 <s4251447@student.rmit.edu.au>
2026-09-18 23:58:25 +1000

    Add initial game setup and assets for Flappy Bird AI

---
+++ flappybird.py
@@ -0,0 +1,36 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRDS_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
+PIPES_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
+
+class Bird:
+    IMGS = BIRDS_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x,y ):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.velocity = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.velocity = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
