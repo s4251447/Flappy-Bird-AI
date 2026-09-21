96ad5b35dc80de6fc4bea36cb688c67ab0bf6502
s4251447 <s4251447@student.rmit.edu.au>
2026-09-21 12:59:57 +1000

    Refactor bird movement and drawing logic; adjust window width

--- flappybird.py
+++ flappybird.py
@@ -3,8 +3,8 @@ import neat
 import time
 import os
 import random
-
-WIN_WIDTH = 600
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 BIRDS_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
@@ -34,3 +34,77 @@ class Bird:
         self.height = self.y
 
     def move(self):
+        self.tick_count += 1
+
+        displacement = self.velocity * self.tick_count + 1.5 * self.tick_count ** 2
+
+        if displacement >= 16:
+            displacement = 16
+
+        if displacement < 0:
+            displacement -= 2
+
+        self.y = self.y + displacement
+
+        if displacement < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+def draw_window(win, birds):
+    win.blit(BG_IMG, (0,0))
+    for bird in birds:
+        bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    birds = [Bird(200, 200)]
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        for bird in birds:
+            bird.move()
+        draw_window(win, birds)
+
+
+    pygame.quit()
+    quit()
+
+main()
